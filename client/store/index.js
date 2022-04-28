8dc7f2006e78902ba0d336b09901d309c2e111db
Caitlin Pauze Sherman <caitlinpauzesherman@Caitlins-MacBook-Pro.local>
2022-04-27 21:09:53 -0400

    game store properly hooked up in index now

--- client/store/index.js
+++ client/store/index.js
@@ -2,7 +2,8 @@ import {createStore, combineReducers, applyMiddleware} from 'redux'
 import {createLogger} from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
-import gameReducer from './game'
+import game from './game'
+
 
 const reducer = combineReducers({ game })
 const middleware = composeWithDevTools(
